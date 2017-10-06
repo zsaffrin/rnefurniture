432fea0c358d2191e3774a1b2be6c0ee7c6c5b84
Zach Saffrin <zach@zachsaffrin.com>
2017-10-06 15:50:23 -0400

    Organize structure

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Header from './Header/Header';
+
+const App = () => (
+	<div className="App">
+                <Header />
+                <p>Version 4.0 in progress</p>
+	</div>
+);
+
+export default App;
