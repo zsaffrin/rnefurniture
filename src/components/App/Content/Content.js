81f49f0af77d9173b64174b2a8f28d0a4f9c7f34
Zach Saffrin <zach@zachsaffrin.com>
2017-10-18 21:45:07 -0400

    Use new Gallery, Contact, and Education pages

--- src/components/App/Content/Content.js
+++ src/components/App/Content/Content.js
@@ -3,12 +3,18 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import Container from '../Container/Container';
 import Home from './Home/Home';
 import About from './About/About';
+import Gallery from './Gallery/Gallery';
+import Contact from './Contact/Contact';
+import Education from './Education/Education';
 
 const Content = () => (
 	<Container>
 		<Switch>
 			<Route exact path="/" component={Home} />
 			<Route path="/about" component={About} />
+			<Route path="/gallery" component={Gallery} />
+			<Route path="/contact" component={Contact} />
+			<Route path="/education" component={Education} />
 			<Redirect to="/" />
 		</Switch>
 	</Container>
