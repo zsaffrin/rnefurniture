527b8368c4ac75ba75c90e1b18d4146ed47c6160
Zach Saffrin <zach@zachsaffrin.com>
2017-10-28 23:13:04 -0400

    Add prop-types definitions

--- src/components/App/Container/Container.js
+++ src/components/App/Container/Container.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { arrayOf, element, oneOfType } from 'prop-types';
 import './Container.css';
 
 const Container = ({ children }) => (
-	<div className="Container">
-        	{children}
-	</div>
+    <div className="Container">
+        {children}
+    </div>
 );
+Container.propTypes = {
+    children: oneOfType([
+        arrayOf(element),
+        element,
+    ]),
+};
+Container.defaultProps = {
+    children: [],
+};
 
 export default Container;
