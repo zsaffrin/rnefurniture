f31a3fe0261b73dc2482bad196df3164226f154e
Zach Saffrin <zach@zachsaffrin.com>
2017-10-29 21:35:08 -0400

    Have nav items react to hover and active states

--- src/components/App/Header/Nav/Nav.js
+++ src/components/App/Header/Nav/Nav.js
@@ -3,18 +3,46 @@ import { Link } from 'react-router-dom';
 import Container from '../../Container/Container';
 import './Nav.css';
 
-const Nav = () => (
-    <div className="Nav">
-        <Container>
-            <div className="Nav-items">
-                <Link to="/" className="Nav-item">Home</Link>
-                <Link to="/about" className="Nav-item">About</Link>
-                <Link to="/gallery" className="Nav-item">Gallery</Link>
-                <Link to="/contact" className="Nav-item">Contact</Link>
-                <Link to="/education" className="Nav-item">Training/Classes</Link>
-            </div>
-        </Container>
-    </div>
-);
+const Nav = () => {
+    const curPath = window.location.pathname;
+
+    return (
+        <div className="Nav">
+            <Container>
+                <div className="Nav-items">
+                    <Link to="/" className="Nav-item" >Home</Link>
+
+                    <Link
+                        to="/about"
+                        className={`Nav-item ${curPath === '/about' ? 'active' : ''}`}
+                    >
+                        About
+                    </Link>
+
+                    <Link
+                        to="/gallery"
+                        className={`Nav-item ${curPath === '/gallery' ? 'active' : ''}`}
+                    >
+                        Gallery
+                    </Link>
+
+                    <Link
+                        to="/contact"
+                        className={`Nav-item ${curPath === '/contact' ? 'active' : ''}`}
+                    >
+                        Contact
+                    </Link>
+
+                    <Link
+                        to="/education"
+                        className={`Nav-item ${curPath === '/education' ? 'active' : ''}`}
+                    >
+                        Training/Classes
+                    </Link>
+                </div>
+            </Container>
+        </div>
+    );
+};
 
 export default Nav;
