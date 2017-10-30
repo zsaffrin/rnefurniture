1e3d8a425d2080de2644792edd2fbdd0bb85564e
Zach Saffrin <zach@zachsaffrin.com>
2017-10-30 16:11:30 -0400

    Polish footer layout and styling, add active links to author and colophon pages

--- src/components/App/Footer/Footer.js
+++ src/components/App/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Container from '../Container/Container';
 import './Footer.css';
 
@@ -9,8 +10,18 @@ const Footer = () => (
                 All images &copy; RNE Furniture
             </div>
             <div className="Footer-col">
-                Site design by Zach Saffrin<br />
-                Full site credits / Colophon
+                Site design by&nbsp;
+                <a
+                    href="http://zachsaffrin.com"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="subtle-link"
+                >
+                    Zach Saffrin
+                </a><br />
+                <Link to="/colophon" className="subtle-link">
+                    Full site credits / Colophon
+                </Link>
             </div>
         </footer>
     </Container>
